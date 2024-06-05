44ba2a9ad5f2c59087db255789412a94fe36f0cc
MaxChavezSegura <maxchseg@gmail.com>
2024-06-05 14:47:43 -0700

    Updating the App before adding the Service

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { Housinglocation } from '../housinglocation';
+
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [
+  CommonModule
+],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
+
   @Input() housingLocation!: Housinglocation;
+  
+
 }
